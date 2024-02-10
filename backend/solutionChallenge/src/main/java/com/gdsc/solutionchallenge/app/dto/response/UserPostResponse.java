c1015a8f92b20e098bfb72942ec5945b30d0204c
koo995 <107671886+koo995@users.noreply.github.com>
2024-02-10 12:18:25 +0900

    Fix: Return UserPostResponse dto

--- backend/solutionChallenge/src/main/java/com/gdsc/solutionchallenge/app/dto/response/UserPostResponse.java
+++ backend/solutionChallenge/src/main/java/com/gdsc/solutionchallenge/app/dto/response/UserPostResponse.java
@@ -1,4 +1,23 @@
 package com.gdsc.solutionchallenge.app.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class UserPostResponse {
+
+    private Long postId;
+
+    private String livingThings;
+
+    private String scientificName;
+
+    // todo 학명을 한국어로 디비에서 매칭시켜서 전달.
+
+    public UserPostResponse(Long postId, String livingThings, String scientificName) {
+        this.postId = postId;
+        this.livingThings = livingThings;
+        this.scientificName = scientificName;
+    }
 }
