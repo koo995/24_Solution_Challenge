56041516ce0a563bbf63f650a23e0db63eb5455a
koo995 <107671886+koo995@users.noreply.github.com>
2024-02-17 06:01:26 +0900

    fix: Redirect url when 200

--- backend/solutionChallenge/src/main/java/com/gdsc/solutionchallenge/mission/controller/MissionController.java
+++ backend/solutionChallenge/src/main/java/com/gdsc/solutionchallenge/mission/controller/MissionController.java
@@ -5,42 +5,50 @@ import com.gdsc.solutionchallenge.member.domain.Member;
 import com.gdsc.solutionchallenge.mission.dto.request.MissionCreateDto;
 import com.gdsc.solutionchallenge.mission.dto.response.MissionDetail;
 import com.gdsc.solutionchallenge.mission.dto.response.MissionListResponse;
+import com.gdsc.solutionchallenge.mission.dto.response.MissionResult;
 import com.gdsc.solutionchallenge.mission.service.MissionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class MissionController {
 
     private final MissionService missionService;
 
     @PostMapping("/api/v1/mission")
-    public Long create(@RequestBody MissionCreateDto missionCreateDto, @Login Member loginMember) {
-        return missionService.createMission(missionCreateDto, loginMember);
+    public String create(@RequestBody MissionCreateDto missionCreateDto, @Login Member loginMember, RedirectAttributes redirectAttributes) {
+        Long missionId = missionService.createMission(missionCreateDto, loginMember);
+        redirectAttributes.addAttribute("missionId", missionId);
+        return "redirect:/api/v1/mission/{missionId}";
     }
 
+    @ResponseBody
     @GetMapping("/api/v1/mission")
     public List<MissionListResponse> list(@Login Member loginMember) {
         List<MissionListResponse> response = missionService.getList(loginMember);
         return response;
     }
 
+    @ResponseBody
     @GetMapping("/api/v1/mission/{missionId}")
     public MissionDetail detail(@PathVariable(name = "missionId") Long missionId) {
         return missionService.detail(missionId);
     }
 
+    @ResponseBody
     @PostMapping("/api/v1/mission/{missionId}")
-    public Long upload(@PathVariable(name = "missionId") Long missionId,
-                       @Login Member loginMember,
-                       @RequestParam("file") MultipartFile file) {
+    public MissionResult upload(@PathVariable(name = "missionId") Long missionId,
+                                @Login Member loginMember,
+                                @RequestParam("file") MultipartFile file) {
         Long imageId = missionService.imageUpload(missionId, loginMember, file);
-        return imageId;
+        return new MissionResult(imageId);
     }
 }
