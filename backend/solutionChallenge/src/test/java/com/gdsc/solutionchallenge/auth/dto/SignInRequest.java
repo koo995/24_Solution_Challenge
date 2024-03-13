96ab86a36b911d65f87e1a9bec181b5088af0ee5
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-14 04:24:50 +0900

    test: Add signIn dto for firebase authentication.

---
+++ backend/solutionChallenge/src/test/java/com/gdsc/solutionchallenge/auth/dto/SignInRequest.java
@@ -0,0 +1,18 @@
+package com.gdsc.solutionchallenge.auth.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class SignInRequest {
+    private String email;
+    private String password;
+    private Boolean returnSecureToken;
+
+    @Builder
+    public SignInRequest(String email, String password, Boolean returnSecureToken) {
+        this.email = email;
+        this.password = password;
+        this.returnSecureToken = returnSecureToken;
+    }
+}
