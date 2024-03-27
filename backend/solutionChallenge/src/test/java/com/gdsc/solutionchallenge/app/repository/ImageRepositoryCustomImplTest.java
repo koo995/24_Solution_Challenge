a98968e075d3f8a18c845cbc70ec8fe8353f2d21
koo995 <107671886+koo995@users.noreply.github.com>
2024-03-27 17:14:42 +0900

    test: Add unit test

--- backend/solutionChallenge/src/test/java/com/gdsc/solutionchallenge/app/repository/ImageRepositoryCustomImplTest.java
+++ backend/solutionChallenge/src/test/java/com/gdsc/solutionchallenge/app/repository/ImageRepositoryCustomImplTest.java
@@ -81,4 +81,55 @@ class ImageRepositoryCustomImplTest extends IntegrationTestSupport {
             assertThat(content.getFullPath()).isNotNull();
         }
     }
+
+    @DisplayName("조건이 animal 일 때, 멤버 아이디를 통해 이미지를 조회한다.")
+    @Test
+    void findAnimalImageByMemberId() throws Exception {
+        // given
+        Member member = Member.builder()
+                .uid("12345-asdf")
+                .email("gunhong951@gmail.com")
+                .username("koo")
+                .build();
+        ArrayList<Species> speciesList = new ArrayList<>();
+        for (int i = 0; i < 20 ; i++ ) {
+            Species species = Species.builder()
+                    .kingdom(i % 2 == 0 ? "animal" : "plant")
+                    .koreaName("korName" + i)
+                    .scientificName("sciName" + i)
+                    .build();
+            speciesList.add(species);
+        }
+        ArrayList<Image> imageList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            Image image = Image.builder()
+                    .fullPath("/*/" + i)
+                    .uploadFileName("fileName" + i)
+                    .latLng(LatLng.newBuilder()
+                            .setLatitude(36.5)
+                            .setLongitude(124.5).build())
+                    .build();
+            image.setSpecies(speciesList.get(i));
+            image.setMember(member);
+        }
+        memberRepository.save(member);
+        speciesRepository.saveAll(speciesList);
+        imageRepository.saveAll(imageList);
+
+        Pageable pageable = PageRequest.of(0, 10);
+        FilterCondition filterCondition = new FilterCondition();
+        filterCondition.setKingdom("animal");
+
+        // when
+        PageImpl<ImageDto> imageByMemberId = imageRepository.findImageByMemberId(member.getId(), filterCondition, pageable);
+        List<ImageDto> contents = imageByMemberId.getContent();
+
+        // then
+        assertThat(imageByMemberId.getTotalElements()).isEqualTo(10);
+        assertThat(imageByMemberId.getSize()).isEqualTo(10);
+        for (ImageDto content : contents) {
+            assertThat(content.getImageId()).isNotNull();
+            assertThat(content.getFullPath()).isNotNull();
+        }
+    }
 }
